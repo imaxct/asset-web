6d60e0da2b6de9e1479aa4becc141b4f9ec3c6b7
imaxct <imaxct@hotmail.com>
2019-04-24 10:54:03 +0800

    资产列表页

--- src/view/asset/asset-list.vue
+++ src/view/asset/asset-list.vue
@@ -14,10 +14,15 @@
       :current="number + 1"
       @on-change="onChangeNo"
     ></Page>
+    <Button
+      style="margin-top: 10px;"
+      type="primary"
+      @click="showAddModal"
+    >添加资产</Button>
   </div>
 </template>
 <script>
-import { listAsset, updateAsset } from '@/api/asset'
+import { listAsset, updateAsset, addAsset } from '@/api/asset'
 import { listDep } from '@/api/dep'
 import { PropertyStatus } from '@/libs/util'
 import Tables from '_c/tables'
@@ -35,6 +40,7 @@ export default {
       totalPages: 0,
       totalElements: 0,
       number: 0,
+      edit: [{ name: '', depId: 0, num: 1 }],
       depList: [],
       columns: [
         { title: 'id', key: 'id', sortable: true },
@@ -47,6 +53,10 @@ export default {
               <Tag>{PropertyStatus[params.row.curStatus]}</Tag>
             )
           }
+        },
+        {
+          title: '当前流程',
+          key: 'curProcess'
         },
         {
           title: '部门',
@@ -67,6 +77,10 @@ export default {
           }
         },
         {
+          title: '当前使用人',
+          key: 'occupyUserName'
+        },
+        {
           title: '二维码',
           key: 'propertyId',
           render: (h, params) => {
@@ -79,6 +93,70 @@ export default {
     }
   },
   methods: {
+    showAddModal () {
+      this.$Modal.confirm({
+        render: (h) => {
+          const addItem = (length) => {
+            const result = []
+            for (let i = 0; i < length; i++) {
+              result.push(
+                <FormItem>
+                  <Row>
+                    <Col span={7}><Input placeholder="资产名" v-model={this.edit[i].name}></Input></Col>
+                    <Col span={7}>
+                      <Select placeholder="部门" v-model={this.edit[i].depId}>
+                        {this.depList.map(it => {
+                          return (
+                            <Option value={it.id} key={it.id}>{it.label}</Option>
+                          )
+                        })}
+                      </Select>
+                    </Col>
+                    <Col span={7}><InputNumber min={1} v-model={this.edit[i].num}></InputNumber></Col>
+                    <Col span={3}><Button onClick={() => { this.edit.splice(i, 1) }}>删除</Button></Col>
+                  </Row>
+                </FormItem>
+              )
+            }
+            return result
+          }
+          return (
+            <div>
+              <Form>
+                {addItem(this.edit.length)}
+                <FormItem><Button icon="md-add" onClick={() => this.edit.push({ name: '', depId: 0, num: 1 })}>添加资产</Button></FormItem>
+              </Form>
+            </div>
+          )
+        },
+        onOk: () => {
+          const assetList = []
+          for (let idx in this.edit) {
+            const it = this.edit[idx]
+            for (let num = 1; num <= it.num; num++) {
+              assetList.push({ name: it.name, depId: it.depId })
+            }
+          }
+          addAsset(assetList).then(res => {
+            const { data } = res
+            if (data.ok) {
+              this.$Message.success('添加成功')
+              this.loadPage(this.number, this.size)
+            } else {
+              this.$Message.error(data.msg)
+            }
+          }).catch(err => {
+            this.$Message.error(err)
+          })
+        },
+        onCancel: () => {
+          this.edit = [{ name: '', depId: 0, num: 1 }]
+        }
+      })
+    },
+    onAddAsset () {
+
+    },
     onChange ({ id, name, depId }) {
       updateAsset({ id, name, depId }).then(res => {
         const { data } = res
